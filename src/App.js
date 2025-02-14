eb2c0c8c1b85ac50cfcaa3e1f7619c9a62391287
Chayan jain <chayantwinkle@gmail.com>
2025-02-14 12:01:45 +0530

    tic-toc-tac

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Square from './components/square';
+function App() {
+  return (
+    <div className="container">
+      <Square user="X"/>
+      <Square/>
+      <Square/>
+      <Square/>
+      <Square/>
+      <Square/>
+      <Square/>
+      <Square/>
+      <Square/>   
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/square.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function Square(props) {
+    return (
+        <>
+            <div className="square">
+                {props.user}
+            </div>
+        </>
+    )
+
+}
+
+
+
+export default Square;
